017987e5f3bb2541e6c6dd2a8cdd9cc1bf6333df
josuesss72 <jjdanielsarmiento@gmail.com>
2025-01-09 15:53:59 -0500

    fix: solucion undefined variable entorno

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -1,9 +1,9 @@
-export const { STRAPI_HOST, STRAPI_TOKEN, NEXT_PUBLIC_STRAPI_HOST } = process.env;
+export const { STRAPI_TOKEN, NEXT_PUBLIC_STRAPI_HOST } = process.env;
 
 export async function query(url: string) {
-  return await fetch(`${STRAPI_HOST}/api/${url}`, {
+  return await fetch(`${NEXT_PUBLIC_STRAPI_HOST}/api/${url}`, {
     headers: {
       Authorization: `Bearer ${STRAPI_TOKEN}`,
     },
-  }).then(res => res.json())
+  }).then((res) => res.json());
 }
